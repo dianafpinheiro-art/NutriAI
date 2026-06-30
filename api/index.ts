31fee39361793eb4ff638a6fc4ff45b503ca04e2
diana <dianafpinheiro@gmail.com>
2026-06-30 00:23:23 -0300

    fix: adaptar backend para Vercel serverless + vercel.json rewrites

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,3 @@
 import app from "../server";
-import type { Request, Response } from "express";
 
-export default function handler(req: Request, res: Response) {
-  return app(req, res);
-}
+export default app;
